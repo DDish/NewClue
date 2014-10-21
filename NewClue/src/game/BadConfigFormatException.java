7f5b8b81a5c06185736498961b748f8140cd3fa9
unknown <ddisharo@CT156-42.adit.mines.edu>
2014-10-20 22:02:48 -0600

    Failing tests for computer suggestion added.

--- NewClue/src/game/BadConfigFormatException.java
+++
@@ -1,8 +0,0 @@
-package game;
-
-public class BadConfigFormatException extends Exception {
-	public BadConfigFormatException() {}
-	  public BadConfigFormatException(String message) {
-	    super(message);
-	  }
-	}
